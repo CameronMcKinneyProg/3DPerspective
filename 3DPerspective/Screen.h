96405e65633733434d31aeec5768831acd439bb2
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-04-28 22:11:28 +0100

    Add Screen constructor

--- 3DPerspective/Screen.h
+++ 3DPerspective/Screen.h
@@ -10,5 +10,13 @@ class Screen
 	SDL_Renderer* renderer;
 	std::vector<SDL_FPoint> points;
 
+	Screen()
+	{
+		SDL_Init(SDL_INIT_VIDEO);
+		SDL_CreateWindowAndRenderer(640 * 2, 480 * 2, 0, &window, &renderer);
+		SDL_RenderSetScale(renderer, 2, 2);
+	}
+
+	
 };
 
