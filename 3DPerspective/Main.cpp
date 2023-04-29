6a6cd31fcec1ce2e7a986f89741163c01b6adbf1
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-04-29 11:59:49 +0100

    Add rotate function

--- 3DPerspective/Main.cpp
+++ 3DPerspective/Main.cpp
@@ -11,7 +11,22 @@ struct vec3
 	float x, y, z;
 };
 
+void rotate(vec3& point, float x = 1, float y = 1, float z = 1)
+{
+	float rad = 0;
 
+	rad = x;
+	point.y = cosf(rad) * point.y - sinf(rad) * point.z;
+	point.z = sinf(rad) * point.y + cosf(rad) * point.z;
+
+	rad = y;
+	point.x = cosf(rad) * point.x + sinf(rad) * point.z;
+	point.z = -sinf(rad) * point.x + cosf(rad) * point.z;
+
+	rad = z;
+	point.x = cosf(rad) * point.x - sinf(rad) * point.y;
+	point.y = sinf(rad) * point.x + cosf(rad) * point.y;
+}
 
 void line(Screen& screen, float x1, float y1, float x2, float y2)
 {
@@ -31,15 +46,19 @@ int main(int argc, char* args[])
 {
 	Screen screen;
 
+	/*// draw pixels at random positions
 	for (int i = 0; i < 100; i++)
 	{
 		screen.pixel(rand() % 640, rand() % 480);
-	}
+	}*/
 
+	/*// draw static square
 	line(screen, 100, 100, 200, 100);
 	line(screen, 200, 100, 200, 200);
 	line(screen, 200, 200, 100, 200);
-	line(screen, 100, 200, 100, 100);
+	line(screen, 100, 200, 100, 100);*/
+
+
 
 	while (true)
 	{
